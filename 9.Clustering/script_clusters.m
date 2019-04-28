c1c193750bddac48205d494f41961545d46c45e2
scythk <scythk@gmail.com>
2019-04-27 23:34:50 -0500

    complete clustering file and add mlx file

--- 9.Clustering/script_clusters.m
+++ 9.Clustering/script_clusters.m
@@ -3,39 +3,76 @@ clc;clear;close all;
 
 % input arguement
 % pathTr = [pwd,'/USPS_train.txt'];
-% pathTe = [pwd,'/USPS_test.txt'];
 pathTr = '../DataSets/USPS_train.txt';
-pathTe = '../DataSets/USPS_test.txt';
-ITER = 300;
+ITER = 100;
 
-[dTr,dTe] = deal(load(pathTr),load(pathTe));
-[x,y] = deal(dTr(:,1:end-1),dTr(:,end));
-[xTe,yTe] = deal(dTe(:,1:end-1),dTe(:,end));
+dTr = load(pathTr);
+[X,y] = deal(dTr(:,1:end-1),dTr(:,end));
+[dLen,Dim] = size(X);	% length and dimension of data
+nCls = numel(unique(y));
 % pre-processing
-xhat = (x-mean(x))./std(x);
-xTehat = (xTe-mean(xTe))./std(xTe);
+% Xbar = X-mean(X);
+Xbar = (X-mean(X))./std(X);
 
-[accEucl,accManh] = deal(zeros(length(K),1));
-[yPreEucl,yPreManh] = deal(zeros(length(xTe),1));
-% testing
-for k = 1:length(K)
-    for i = 1:length(xTehat)
-        dEucl = sqrt(sum((xTehat(i,:)-xhat).^2,2));	% Euclidean distance
-        dManh = sum(abs(xTehat(i,:)-xhat),2);       % Manhattan distance
-        [~,kIndEucl] = mink(dEucl,K(k));    % k smallest elements
-        [~,kIndManh] = mink(dManh,K(k));
-        yPreEucl(i) = mode(y(kIndEucl));    % Most frequent value
-        yPreManh(i) = mode(y(kIndManh));
-        fprintf('object ID:%d; Euclidean:%d, Manhattan:%d; true:%d\n',i,yPreEucl(i),yPreManh(i),yTe(i));
+
+%% PCA
+Sigma = Xbar*Xbar'/dLen;
+[V,D] = eig(Sigma);
+save('sigma.mat','V','D');
+
+%% Clustering
+% load('sigma.mat');
+acc = zeros(Dim-1,ITER);
+
+% Assign random class to data
+yRand = randi([0 9],dLen,1);
+
+% testing for different # of principal components
+for p = 1:Dim-1
+    xPCA = V(:,end-p+1:end);
+    yEst = yRand;
+    meanCls = zeros(nCls,size(xPCA,2));
+    for iter = 1:ITER
+        % Calculate mean of each class
+        for i = 1:nCls
+            meanCls(i,:) = mean(xPCA(yEst==(i-1),:));
+        end
+        
+        % Assign new class to data
+        for n = 1:dLen
+            dEucl = sqrt(sum((xPCA(n,:)-meanCls).^2,2));	% distance from means
+            [~,minD] = min(dEucl);  % minimum distance
+            yEst(n) = minD-1;   % new class
+        end
+        clsErr = 0;
+        % calculate accuracy
+        for i = 1:nCls
+            % find dominant class
+            yCls = y(yEst==(i-1));
+            [~,domCls] = max(hist(yCls,(0:9)));	%#ok
+            domCls = domCls-1;
+            % count classification error among all classes
+            clsErr = clsErr+sum(yCls-domCls~=0);
+        end
+        acc(p,iter) = 1-clsErr/dLen;
     end
-    accEucl(k) = sum((yPreEucl-yTe)==0)/length(xTe);
-    accManh(k) = sum((yPreManh-yTe)==0)/length(xTe);
-    fprintf('The accuracy of Euclidean is %f\nThe accuracy of Manhattan is %f\n',accEucl(k),accManh(k));
 end
-% plot
+
+%% plot
+legendStr = cell(Dim-1,1);
 figure
-plot(K,accEucl,'ro-');hold on;
-plot(K,accManh,'b*-');hold on;
+for p = 1:Dim-1
+    plot(1:ITER,acc(p,:),'LineWidth',2);hold on;
+    legendStr{p} = [num2str(p),' components'];
+end
 grid on;
-xlabel('K-Nearest-Neighbor');ylabel('Clustering accuracy');
-legend('Euclidean','Manhattan');
+title('Clustering accuracy vs iteration');
+xlabel('iteration');ylabel('Clustering accuracy');
+legend(legendStr);
+% saveas(gcf,'AccVsIter.png');
+
+figure
+plot(1:Dim-1,acc(:,end),'ro-');hold on;grid on;
+title('Clustering accuracy vs # of components');
+xlabel('# of components');ylabel('Clustering accuracy');
+% saveas(gcf,'AccVsComp.png');
